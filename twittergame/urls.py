a5e8a4d9d67e65f63ca8ed11c18c31229833a36d
Shayan <Shamousavinia@gmail.com>
2021-05-09 11:19:11 +0430

    add token

--- twittergame/urls.py
+++ twittergame/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     path('account/', include('accounts.urls')),
     path('account/', include('Post.urls')),
     path('account/', include('GameBar.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    
 
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
