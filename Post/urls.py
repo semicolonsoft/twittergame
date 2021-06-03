f8ec14199810f30c16c56e8d15d8474ca7f6579a
Shayan <Shamousavinia@gmail.com>
2021-06-03 16:38:47 +0430

    Fix Some Errors

--- Post/urls.py
+++ Post/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('Posts', Posts),
     path('Replays', Replays),
     path('Like', Like),
+    path('ExtLike', ExtLike),
 ]
--- RecommendPost/admin.py
+++ RecommendPost/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
 
-from . models import seenPostClass
-
-admin.site.register(seenPostClass)
+# Register your models here.
